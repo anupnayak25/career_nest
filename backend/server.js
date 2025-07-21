4f79f2352d4c3434284652227d6b48ba5fdd6026
mrnayak25 <mrnayak27@gmail.com>
2025-07-21 23:21:42 +0530

    fixes

--- backend/server.js
+++ backend/server.js
@@ -24,22 +24,7 @@ process.on("unhandledRejection", (reason, promise) => {
 });
 
 // ======= Middleware Setup =======
-
-// Allow localhost and production domain
-const allowedOrigins = ['http://localhost:57722', 'https://careerernest.anupnayak.me'];
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
